6409abd8006e0723973bfdf24adf429551abac34
Yan Kwong <yantkwong@gmail.com>
2018-11-25 11:31:41 -0800

    added finished basic codes

--- learnBasics.js
+++ learnBasics.js
@@ -1,25 +1,46 @@
 'use strict';
-
 function addTen(number) {
-    
+    return number + 10;
 }
 
 function doAdd(numberA, numberB) {
-
+    return numberA + numberB;
 }
 
 function doMultiply(numberA, numberB) {
+    return numberA * numberB;
+}
 
+function addOne(i) {
+    return i + 1;
 }
 
 // conditional, IF
-function isLessThanTen(parameter) {
-
+function isLessThanTen(userInput) {
+    if (userInput < 10) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-// loop: for
-function doAddTenThreeTimes(number) {
+function isThisPersonAGirl(something) {
+    if(something == 'girl'){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+
+// loop: for
+function doAddTenThreeTimes(i) {
+     for(var j = 0;  j < 3;  j++) {
+         i = i + 10; //this is what's being run
+     }
+     return i;
 }
 
 // array
@@ -27,11 +48,43 @@ function getFirstElement(parameterArray) {
 
 }
 
+function determineSize(pound) {
+    if (pound <= 10) {
+        return 'small';
+    }
+    else if (pound >= 11 && pound <= 20) {
+        return 'medium';
+    }
+    else {
+        return 'large'
+    }
+}
+
+function addMoonMoonMoonToUserInput(userInput) {
+   for(var j = 0; j < 3 ; j++){
+       userInput = userInput + 'Moon' 
+   }
+    return userInput 
+}
+
+function concatUserInputWithNumber(userInput, start, finish) {
+    var resultString = '';
+
+    for(var numberToConcat=start; numberToConcat <= finish; numberToConcat++){
+        resultString = resultString +  (userInput + numberToConcat);
+    }
+    return resultString;
+}
+
 module.exports = {
     addTen,
     doAdd,
+    addOne,
+    isThisPersonAGirl,
     doMultiply,
     isLessThanTen,
     doAddTenThreeTimes,
-    getFirstElement
+    getFirstElement,
+    determineSize,
+    addMoonMoonMoonToUserInput
 }
--- tests/basics.test.js
+++ tests/basics.test.js
@@ -12,33 +12,45 @@ describe.only('The Basic Concepts', () => {
         it('should add 10 to a negative number',() => {
             assert.equal(learnBasics.addTen(-5), 5);
         });
+    }); 
+
+    context('AddOne', ()=> {
+        it('should add 1 to a positive number', ()=>{
+            assert.equal(learnBasics.addOne(5), 6);
+        });
+        it('should add 1 to a negative number', () => {
+            assert.equal(learnBasics.addOne(-1), 0);
+        });
+        it('should add 1 to a zero', () => {
+            assert.equal(learnBasics.addOne(0), 1);
+        });
     });
 
-    context.skip('doAdd', () => {
+    context('doAdd', () => {
         it('should add 8 to 2 and gives us 10', () => {
             assert.equal(learnBasics.doAdd(8, 2), 10);
         });
 
         it('should add 8 to -2 and gives us 6', () => {
-            assert.equal(learnBasics.doAdd(8, -2), 10);
+            assert.equal(learnBasics.doAdd(8, -2), 6);
         });
 
         it('should throw an error when a parameter is not an integer');
     });
 
-    context.skip('doMultiply', () => {
+    context('doMultiply', () => {
         it('should multiply 8 and 2 will gives us 16', () => {
-            assert.equal(learnBasics.doAdd(8, 2), 16);
+            assert.equal(learnBasics.doMultiply(8, 2), 16);
         });
 
         it('should multiply 8 and -2 will gives us -16', () => {
-            assert.equal(learnBasics.doAdd(8, -2), -16);
+            assert.equal(learnBasics.doMultiply(8, -2), -16);
         });
 
         it('should throw an error when a parameter is not an integer');
     });
 
-    context.skip('isLessThanTen', ()=> {
+    context('isLessThanTen', ()=> {
         it('should return true when given a number', ()=> {
             assert.equal(learnBasics.isLessThanTen(5), true);
         });
@@ -48,13 +60,48 @@ describe.only('The Basic Concepts', () => {
         });
     });
 
-    context.skip('doAddTenThreeTimes', ()=> {
+    context('isThisPersonAGirl', ()=> {
+        it('should return true when given a girl', ()=> {
+            assert.equal(learnBasics.isThisPersonAGirl('girl'), true);
+        });
+
+        it('should return false when given a boy', ()=> {
+            assert.equal(learnBasics.isThisPersonAGirl('boy'), false);
+        });
+
+        it('should return false when given a computer', ()=> {
+            assert.equal(learnBasics.isThisPersonAGirl('asfsdf'), false);
+        });
+    });
+
+    context('determineSize', ()=> {
+        it('should return the word "small" when size is less than 10', ()=>{
+            assert.equal(learnBasics.determineSize(9), 'small');
+        });
+        it('should return the word "small" when size is less than 10', ()=>{
+            assert.equal(learnBasics.determineSize(10), 'small');
+        });
+        it('should return the word "medium" when size is between 11 to 20', ()=>{
+            assert.equal(learnBasics.determineSize(11), 'medium');
+        });
+        it('should return the word "large" when size is bigger than 20', ()=>{
+            assert.equal(learnBasics.determineSize(21), 'large');
+        });
+    });
+
+    context('doAddTenThreeTimes', ()=> {
         it('should return 40 when given 10', ()=> {
             assert.equal(learnBasics.doAddTenThreeTimes(10), 40);
         });
 
         it('should return 30 when given 0', ()=> {
             assert.equal(learnBasics.doAddTenThreeTimes(0), 30);
+        });
+    });
+
+    context('addMoonMoonMoonToUserInput', ()=> {
+        it('should return user string with MoonMoonMoon appended', ()=> {
+            assert.equal(learnBasics.addMoonMoonMoonToUserInput('bobby'), 'bobbyMoonMoonMoon');
         });
     });
 
